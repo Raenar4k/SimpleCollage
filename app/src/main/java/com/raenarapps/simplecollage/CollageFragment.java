bc0d6d2d48092a7c1cb45621e5621376cd9e8cf6
Raenar4k <RaenarDev@gmail.com>
2016-03-21 17:01:55 +0200

    changed collage layout, uses PercentRelativeLayout

--- app/src/main/java/com/raenarapps/simplecollage/CollageFragment.java
+++ app/src/main/java/com/raenarapps/simplecollage/CollageFragment.java
@@ -6,16 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 
 public class CollageFragment extends Fragment {
+    Button buttonDraw;
+    private ImageView imageView1;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ImageView imageView1;
         ImageView imageView2;
         ImageView imageView3;
         ImageView imageView4;
@@ -26,10 +29,27 @@ public class CollageFragment extends Fragment {
         imageView4 = (ImageView) rootView.findViewById(R.id.imageView4);
         String[] urlArray = getArguments().getStringArray(Utility.SELECTED_URLS_ARRAY);
 
-        Picasso.with(getContext()).load(urlArray[0]).into(imageView1);
-        Picasso.with(getContext()).load(urlArray[1]).into(imageView2);
-        Picasso.with(getContext()).load(urlArray[2]).into(imageView3);
-        Picasso.with(getContext()).load(urlArray[3]).into(imageView4);
+        Picasso.with(getContext()).load(urlArray[0])
+                .fit().centerCrop()
+                .into(imageView1);
+        Picasso.with(getContext()).load(urlArray[1])
+                .fit().centerCrop()
+                .into(imageView2);
+        Picasso.with(getContext()).load(urlArray[2])
+                .fit().centerCrop()
+                .into(imageView3);
+        Picasso.with(getContext()).load(urlArray[3])
+                .fit().centerCrop()
+                .into(imageView4);
+
+        buttonDraw = (Button) rootView.findViewById(R.id.buttonDraw);
+        buttonDraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return rootView;
     }
 }
